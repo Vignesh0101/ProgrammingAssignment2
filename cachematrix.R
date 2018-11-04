d1c1422a241f906ad9269a1c8d8a34c2119c2630
Vignesh0101 <43517038+Vignesh0101@users.noreply.github.com>
2018-11-04 10:45:33 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## Set function assigns the inverse of a matrix to a global vector via "<<-" operator (making a cache)
 
+makeCacheMatrix <- function(x = matrix()) 
+{
+  invmat <- NULL
+  set <- function(y)
+  {
+    x <<- y
+    invmat <<- NULL
+  }
+  #Get matrix function
+  get <- function() x
+  
+  #Set inverse of the matrix function
+  setinverse <- function(solve) invmat <<- solve
+  getinverse <- function() invmat
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
 
 
-## Write a short comment describing this function
+## If inverse is already available, it returns it else calculates the inverse
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+  #get the inverse of the matrix
+  inversemat <- x$getinverse()
+  
+  #check if the cached value exists
+  if(!is.null(inversemat))
+  {
+    message("Getting cached Inverse Matrix")
+    return(inversemat)
+  }
+  
+  data <- x$get()
+  #if the cached value does not exist, do the inverse.
+  inversemat <- solve(data, ...)
+  x$setinverse(inversemat)
+  inversemat
 }
